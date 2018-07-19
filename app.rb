78195f95817ecb19f67227a6ddd967c2055836e7
Anu Khambete <anukhambete@gmail.com>
2018-07-19 10:54:37 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
   get "/reversename/:name" do
-    @reverse = params[:name].to_i * params[:num2].to_i
+    @reverse = params[:name].reverse
     "#{@product}"
   end
 
