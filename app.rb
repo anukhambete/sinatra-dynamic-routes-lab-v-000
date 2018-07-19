3a49878f3fa953f947de8006f9922e323d40c63b
Anu Khambete <anukhambete@gmail.com>
2018-07-19 11:18:30 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -20,4 +20,11 @@ class App < Sinatra::Base
     "#{@phrase}"*@number
   end
 
+  get "/say/:word1/:word2/:word3/:word4/:word5" do
+    @number = params[:number].to_i
+    @phrase = params[:phrase]
+
+    "#{@phrase}"*@number
+  end
+
 end
