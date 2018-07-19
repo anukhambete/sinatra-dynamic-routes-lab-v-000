4e3ce9e172bb81927e297a8df0b51fec3053367c
Anu Khambete <anukhambete@gmail.com>
2018-07-19 10:54:08 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,8 +3,8 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
   get "/multiply/:num1/:num2" do
-      @product = params[:num1].to_i * params[:num2].to_i
-      "#{@product}"
+    @product = params[:num1].to_i * params[:num2].to_i
+    "#{@product}"
   end
-    
+
 end
