ee01c6f38cf3f05b08d652a930304cb8b5c82934
Anu Khambete <anukhambete@gmail.com>
2018-07-19 11:16:56 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -16,10 +16,8 @@ class App < Sinatra::Base
   get "/say/:number/:phrase" do
     @number = params[:number].to_i
     @phrase = params[:phrase]
-    5.times do |i|
-      print i, " "
-    end
-    #xyz"#{@phrase}"*@number
+
+    {@phrase}"*@number
   end
 
 end
