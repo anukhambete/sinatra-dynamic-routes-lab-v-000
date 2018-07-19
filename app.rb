3d255f20f4655e22b8c2f31b5e01af4b71c9e1f7
Anu Khambete <anukhambete@gmail.com>
2018-07-19 10:54:27 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,8 +2,8 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
-  get "/multiply/:num1/:num2" do
-    @product = params[:num1].to_i * params[:num2].to_i
+  get "/reversename/:name" do
+    @reverse = params[:num1].to_i * params[:num2].to_i
     "#{@product}"
   end
 
